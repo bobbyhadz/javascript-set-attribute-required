47f4f176dbfbc548923ab0120afa03b4f2506b16
bobbyhadz <opmftw@gmail.com>
2024-03-05 07:09:40 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+const input = document.getElementById('first_name');
+
+// ✅ Set required attribute
+input.setAttribute('required', '');
+
+// ✅ Remove required attribute
+// input.removeAttribute('required');
